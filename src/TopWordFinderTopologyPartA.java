c2cd3d0d82e1f2b93c743220a705d8104f4d8ad6
anton.filimonov <anton.filimonov@akqa.com>
2015-09-08 16:48:30 +0300

    updted TopWordFinderTopologyPartA

--- src/TopWordFinderTopologyPartA.java
+++ src/TopWordFinderTopologyPartA.java
@@ -25,7 +25,7 @@ public class TopWordFinderTopologyPartA {
     Config config = new Config();
     config.setDebug(true);
 
-    builder.setSpout("spout", new RandomSentenceSpout(), 1);
+    builder.setSpout("spout", new RandomSentenceSpout(), 5);
     builder.setBolt("split", new SplitSentenceBolt(), 8).shuffleGrouping("spout");
     builder.setBolt("count", new WordCountBolt(), 12).fieldsGrouping("split", new Fields("word"));
 
